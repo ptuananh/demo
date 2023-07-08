7f4864f27c4db3be5b84471e2f727505d706c8d5
Anh Pham <anhpham1107@gmail.com>
2023-07-08 12:04:01 +0700

    update pakage

---
+++ src/global-settings/global_settings.ts
@@ -0,0 +1,12 @@
+
+export class GlobalSettings {
+	static Settings = {
+		Title: 'Demo',
+		Server: 'https://selidaexxontestimportuser.360awareqa.com',
+		envName: 'prod',
+		version: {
+			name: 'Demo',
+			version: '0.0.0.3'
+		}
+	}
+}
